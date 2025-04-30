f0e181d147e0e0b4c01463a6ddb1e6a835a81b06
Rukhsaranwar <60430263+Rukhsaranwar@users.noreply.github.com>
2025-04-30 18:11:31 +0530

    Update test_script.py

--- test_script.py
+++ test_script.py
@@ -1,7 +1,7 @@
 # test_script.py
 
 def say_hello():
-    print("Hello from Jenkins test script!, v1")
+    print("Hello from Jenkins test script!, v2")
 
 if __name__ == "__main__":
     say_hello()
